392fd8c373bdabaa7a424b01a31f8bbd8572e6eb
Samuel jorge matheus <115334920+samueljmatheus@users.noreply.github.com>
2023-01-27 11:06:16 -0300

    natacao

---
+++ natação.js
@@ -0,0 +1,13 @@
+var idade = 33
+
+if (idade >=5 && idade <=7){
+ console.log("categoria Infantil")   
+} if (idade >=8 && idade <=10) {
+    console.log("categoria Juvenil Nível 1")
+} if (idade >=11 && idade <=15) {
+    console.log("categoria Juvenil Nível 2")
+} if (idade >=16 && idade <=30) {
+    console.log("Categoria Adulta!!!")
+} if(idade>30) {
+    console.log("Categoria Sênior!!!")
+}
